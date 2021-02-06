1b1c46220f7b94b6edf85518aab6d14625da34bc
Lucas Burns <burnsac@me.com>
2021-02-06 15:26:48 -0600

    wrote functions - testing fail2ban

--- riot-headlines/riot-scrape.py
+++ riot-headlines/riot-scrape.py
@@ -16,92 +16,186 @@
 # +
 # =======================================================
 # NOTE: Piping in Python (similar to %>% in R, | in Unix)
-from toolz import pipe
-# import dfply                          # similar to dplyr / has a pipe
-from sspipe import p, px
-from fn import F, _
-import math
-
-pipe(12, math.sqrt, str)                # toolz
-12 | p(math.sqrt) | px ** 2 | p(str)    # sspipe
-(F(math.sqrt) >> _**2 >> str)(12)       # fn
-# =======================================================
-
-# +
-from bs4 import BeautifulSoup as bs
-import requests
-import ssl
-import re
 from datetime import datetime
+from itertools import chain
+import math
+import re
+import ssl
 
+from bs4 import BeautifulSoup as bs
+from fn import F, _
 import numpy as np
 import pandas as pd
+import requests
+from sspipe import p, px
+from toolz import pipe
+import tabloo
+
+# import dfply                            # like dplyr
+
+pipe(12, math.sqrt, str)  # toolz
+12 | p(math.sqrt) | px ** 2 | p(str)  # sspipe
+(F(math.sqrt) >> _ ** 2 >> str)(12)  # fn
+# -
+
 
 # +
 url = "https://slashdot.org/"
 
 headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9,es;q=0.8',
-    #'cache-control': 'max-age=0',
-    'dnt': '1',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-user': '?1',
-    'sec-gpc': '1',
-    'upgrade-insecure-requests': '1',
-    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36'
-    }
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,es;q=0.8",
+    # 'cache-control': 'max-age=0',
+    "dnt": "1",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "cross-site",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36",
+}
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 r = requests.get(url, headers=headers)
-soup = bs(r.text, 'html.parser')
+soup = bs(r.text, "html.parser")
+
+#### ┌─────────────────────────────────────────────────┐
+#### │ Scraping one page                               │
+#### └─────────────────────────────────────────────────┘
 
 # +
 ### Title, link, rating
-tit = [t.get_text() for t in soup.select('h2 span a')]
+tit = [t.get_text() for t in soup.select("h2 span a")]
 
-### Title of post
-title = [j for i in soup.find_all('span', class_='story-title') for j in i.find('a')]
+# Title of post
+title = [j for i in soup.find_all("span", class_="story-title") for j in i.find("a")]
 
-### Date/time of post
-dated = [d.get_text() for d in soup.select('span.story-byline time')]
-date = [re.sub('on|@', '', x).strip() for x in dated]
+# Date/time of post
+dated = [d.get_text() for d in soup.select("span.story-byline time")]
+date = [re.sub("on|@", "", x).strip() for x in dated]
 dt = [datetime.strptime(d, "%A %B %d, %Y %I:%M%p") for d in date]
 
-### External link to post
-elink = [l.text.strip() for l in soup.select('h2 span span')]
+# External link to post
+elink = [l.text.strip() for l in soup.select("h2 span span")]
 
-### Votes on post
-votes = [v.text.strip() for v in soup.find_all('span', class_='comment-bubble')]
+# Votes on post
+votes = [v.text.strip() for v in soup.find_all("span", class_="comment-bubble")]
 
-### Category of post
-classification = [b.get('alt') for b in soup.find_all('img')]
-c = list(filter(None, classification))
+# Category of post
+cat = [b.get("alt") for b in soup.find_all("img")] | p(list, p(filter, None, px))
 # Using this sort of as a try except in case it were to not exist
-category = [x.replace('Icon', '') for x in c] | p(filter, None) | p(list)
+category = [x.replace("Icon", "") for x in cat] | p(filter, None) | p(list)
 
-### User who made the post
-user = [u.get_text() for u in soup.select('span.story-byline a')]
+# User who made the post
+user = [
+    u.get_text(" ", strip=True).replace("\n", "").replace("\t", "")
+    for u in soup.select("span.story-byline")
+]
+user = [" ".join(a.split()) | p(re.findall, r"Postedby\s(\w+)", px) for a in user]
 
-aa = [u.get_text() for u in soup.select('span.story-byline')]
-"Posted" in aa
-
-import grep
-
-len(aa)
-
-### Popularity of post (ratings? red?)
-onclick = [re.findall("'([a-zA-Z0-9,\s]*)'", prop['onclick']) for prop in soup.find_all("span", attrs={"alt":"Popularity"})]
-pop = [p[1] for p in onclick]
+# Popularity of post (ratings? red?)
+pop = [
+    re.findall("'([a-zA-Z0-9,\s]*)'", prop["onclick"]) | px[1]
+    for prop in soup.find_all("span", attrs={"alt": "Popularity"})
+]
 # -
 
+#### ┌─────────────────────────────────────────────────┐
+#### │ Dataframe                                       │
+#### └─────────────────────────────────────────────────┘
+
+# +
+df = pd.DataFrame(
+    {
+        "title": title,
+        "date": dt,
+        "exlink": elink,
+        "category": category,
+        "user": user,
+        "popular": pop,
+    }
+)
+
+tabloo.show(df)
+# from operator import itemgetter
+# df['user'] = df['user'] | p(map, p(itemgetter(0)), px) | p(list)
+
+
+df["user"] = df["user"] | p(chain.from_iterable, px) | p(list)
+df["exlink"] = df["exlink"] | px.str.replace(r"\(|\)", "", regex=True)
+
+#### ┌─────────────────────────────────────────────────┐
+#### │ Scraping more than one page                     │
+#### └─────────────────────────────────────────────────┘
+
+# +
+def get_page(url):
+    response = requests.get(url)
+    if not response.ok:
+        print("Server Responded: ", response.status_code)
+    else:
+        soup = bs(response.text, "lxml")
+    return soup
+
+
+def get_data(soup):
+    try:
+        title = [
+            j for i in soup.find_all("span", class_="story-title") for j in i.find("a")
+        ]
+    except:
+        title = ""
+
+    try:
+        dated = [
+            d.get_text(" ", strip=True) for d in soup.select("span.story-byline time")
+        ]
+        date = [re.sub("on|@", "", x).strip() for x in dated]
+        dt = [datetime.strptime(d, "%A %B %d, %Y %I:%M%p") for d in date]
+    except:
+        dt = ""
+
+    try:
+        elink = [l.text.strip() for l in soup.select("h2 span span")]
+    except:
+        elink = ""
+
+    try:
+        votes = [v.text.strip() for v in soup.find_all("span", class_="comment-bubble")]
+    except:
+        votes = ""
+
+    try:
+        cat = [b.get("alt") for b in soup.find_all("img")] | p(
+            list, p(filter, None, px)
+        )
+        category = [x.replace("Icon", "") for x in cat] | p(filter, None) | p(list)
+    except:
+        category = ""
+
+    try:
+        user = [
+            u.get_text(" ", strip=True).replace("\n", "").replace("\t", "")
+            for u in soup.select("span.story-byline")
+        ]
+        user = [
+            " ".join(a.split()) | p(re.findall, r"Postedby\s(\w+)", px) for a in user
+        ]
+    except:
+        user = ""
+
+    try:
+        pop = [
+            re.findall("'([a-zA-Z0-9,\s]*)'", prop["onclick"]) | px[1]
+            for prop in soup.find_all("span", attrs={"alt": "Popularity"})
+        ]
+    except:
+        pop = ""
 
 
 # +
-pd.DataFrame({'title': title, 'date': dt, 'exlink': elink, 'category': category, 'user': user, 'popular': pop})
